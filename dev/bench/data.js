2d32cc88327671813c3793ba37d0c9595b033631
github-action-benchmark <github@users.noreply.github.com>
2025-03-01 22:29:36 +0000

    add Rust Benchmark (cargo) benchmark result for 61c6e1dbf16a1e33e36ab85cc6bc995d5da69cf4

---
+++ dev/bench/data.js
@@ -0,0 +1,74 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1740868176954,
+  "repoUrl": "https://github.com/SoapSeller/rust-lut",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "dor@shahaf.com",
+            "name": "Dor Shahaf",
+            "username": "SoapSeller"
+          },
+          "committer": {
+            "email": "dor@shahaf.com",
+            "name": "Dor Shahaf",
+            "username": "SoapSeller"
+          },
+          "distinct": true,
+          "id": "61c6e1dbf16a1e33e36ab85cc6bc995d5da69cf4",
+          "message": "Add github actions.",
+          "timestamp": "2025-03-01T23:26:29+01:00",
+          "tree_id": "bd70596653d511a72e332ca8e479494515244c01",
+          "url": "https://github.com/SoapSeller/rust-lut/commit/61c6e1dbf16a1e33e36ab85cc6bc995d5da69cf4"
+        },
+        "date": 1740868176680,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "apply/full_image/1280x720",
+            "value": 14102004,
+            "range": "± 67134",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "process_pixel/color/black",
+            "value": 41,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "process_pixel/color/white",
+            "value": 41,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "process_pixel/color/red",
+            "value": 41,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "process_pixel/color/green",
+            "value": 41,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "process_pixel/color/blue",
+            "value": 41,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "process_pixel/color/gray",
+            "value": 42,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
